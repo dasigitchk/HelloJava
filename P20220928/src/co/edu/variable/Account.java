b4877caedab3aa7963a64ff3bc9224568525a8e9
hyun <pokemdn@naver.com>
2022-09-30 17:41:40 +0900

    0930

--- P20220928/src/co/edu/variable/Account.java
+++ P20220928/src/co/edu/variable/Account.java
@@ -1,7 +1,41 @@
 package co.edu.variable;
 
 public class Account {
-	String accNo; //두단어의 조합시 2번째는 첫번째가 대문자.3번째 4번째 --- 똑같이.
-	String owner; //예금주.
-	int balance; //잔고.
+     String accNo; // 두단어의 조합시 2번째는 첫번째가 대문자.3번째 4번째 --- 똑같이.
+	 String owner; // 예금주.
+	 int balance; // 잔고.
+
+	// setter, getter 정의.0
+//	public void setaccNo(String accNo) {
+//		this.accNo = accNo;
+//	}
+//
+//	String getaccNo() {
+//		return this.accNo;
+//	}
+//
+//	void setowner(String owner) {
+//		
+//	}
+//
+//	String getowner() {
+//		return this.owner;
+//	}
+//
+//	void setbalance(int balance) {
+//
+//	}
+//
+//	int getbalance() {
+//		return this.balance;
+//	}
+//
+//	public String getAccNo() {
+//		return accNo;
+//	}
+//
+//	public void setAccNo(String accNo) {
+//		this.accNo = accNo;
+//	}
+//}
 }
